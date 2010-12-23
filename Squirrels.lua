172658cefce9a43ecefac05727e68a29aca62c63
douglyuckling <douglyuckling@483a7388-aa76-4cae-9b69-882d6fd7d2f9>
2010-12-23 22:26:06 +0000

    Added attacking squirrels
    git-svn-id: https://douglyuckling.svn.beanstalkapp.com/repo/games/snownuts.love@127 483a7388-aa76-4cae-9b69-882d6fd7d2f9

---
+++ Squirrels.lua
@@ -0,0 +1,74 @@
+Squirrels = {first = 0, last = -1}
+Squirrels.mt = {__index = Squirrels}
+
+function Squirrels:new(o)
+    o = o or {}
+    setmetatable(o, Squirrels.mt)
+
+    self._SPEED = 300
+    self._WIDTH = 32
+    self._HEIGHT = 12
+
+    return o
+end
+
+function Squirrels:fire(value)
+  local last = self.last + 1
+  self.last = last
+  self[last] = value
+end
+
+function Squirrels:draw()
+    love.graphics.setColor(102, 51, 0)
+    for i=self.first,self.last do
+        local v = self[i]
+        
+        if v then
+            love.graphics.rectangle("fill",
+                v.x-self._WIDTH/2, v.y-self._HEIGHT/2,
+                self._WIDTH, self._HEIGHT
+            )
+        end
+    end
+    
+    love.graphics.setColor(255, 255, 255)
+    
+    love.graphics.print("first: "..self.first.."  last: "..self.last, 20, 20)
+end
+
+function Squirrels:update(dt)
+    for i=self.first,self.last do
+        if self[i] then break end
+        
+        self.first = self.first + 1
+    end
+
+    for i=self.first,self.last do
+        local v = self[i]
+        
+        if v then
+            v.x = v.x + self._SPEED * dt
+            
+            if v.x > 800+self._WIDTH then
+                self[i] = nil
+            end
+        end
+    end
+
+    for i=self.first,self.last do
+        local squirrel = self[i]
+        
+        if squirrel then
+            for j=lasers.first,lasers.last do
+                local laserPulse = lasers[j]
+                if (laserPulse.x < squirrel.x+self._WIDTH/2) and
+                        (laserPulse.x > squirrel.x-self._WIDTH/2) and
+                        (laserPulse.y < squirrel.y+self._HEIGHT/2) and
+                        (laserPulse.y > squirrel.y-self._HEIGHT/2) then
+                    self[i] = nil
+                end
+            end
+        end
+    end
+end
+
--- main.lua
+++ main.lua
@@ -1,16 +1,20 @@
 require "PlayerPiece"
 require "Lasers"
+require "Squirrels"
 
 bounds = {lowX=0, highX=800, lowY=250, highY=600}
 lastLaserFireTime = -100
+lastSquirrelFireTime= -100
 
 function love.load()
     playerPiece = PlayerPiece:new{position=Vector:new{x=400,y=425}, radius=25, bounds=bounds}
     lasers = Lasers:new()
+    squirrels = Squirrels:new()
 end
 
 function love.draw()
     lasers:draw()
+    squirrels:draw()
     playerPiece:draw()
 end
 
@@ -18,11 +22,17 @@ function love.update(dt)
     playerPiece:update(dt)
     
     local currentTime = love.timer.getTime()
-    if currentTime - lastLaserFireTime > 0.4 then
+    if currentTime - lastLaserFireTime > 0.3 then
 		lasers:fire(playerPiece:getMuzzlePosition())
         lastLaserFireTime = currentTime
     end
     
+    if currentTime - lastSquirrelFireTime > 1 then
+		squirrels:fire(Vector:new{x=0,y=math.random()*(bounds.highY-bounds.lowY)+bounds.lowY})
+        lastSquirrelFireTime = currentTime
+    end
+
     lasers:update(dt)
+    squirrels:update(dt)
 end
 
